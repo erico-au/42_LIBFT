dfd6b3abd4e934f3aaca5baca79f623e29bae557
eraugust <eraugust@student.42sp.org.br>
2022-04-18 21:25:15 +0200

    ft_memcpy

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 21:11:06 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/18 19:39:20 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dest_c;
+	unsigned char	*src_c;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	i = 0;
+	dest_c = (unsigned char *)dest;
+	src_c = (unsigned char *)src;
+	while (i > n)
+	{
+		dest_c[i] = src_c[i];
+		i++;
+	}
+	return (dest);
+}
