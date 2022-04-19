cc2a91d14e611a9bc5c38ecdbd70899a54f86d48
eraugust <eraugust@student.42sp.org.br>
2022-04-19 22:28:35 +0200

    ft_strchr

---
+++ libft/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 21:43:49 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/18 22:21:57 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int		i;
+	char	s;
+
+	s = (unsigned char)c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == s)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (s == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
