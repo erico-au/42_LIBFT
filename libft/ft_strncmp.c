88bb87d853376a11b4775f83947bfb0b56e46dce
eraugust <eraugust@student.42sp.org.br>
2022-04-19 22:27:33 +0200

    ft_strncmp

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 19:00:56 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/19 22:04:31 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	while ((*str1 && str2) != '\0' n > 0)
+	{
+		if (*str1 != *str2)
+			break ;
+		str1++;
+		str2++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	return (*(unsigned char *)str1 - *(unsigned char *)str2);
+}
