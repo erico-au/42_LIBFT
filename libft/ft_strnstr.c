79328d058200bf043495165c268a6240b16d8f76
eraugust <eraugust@student.42sp.org.br>
2022-04-25 19:33:02 +0200

    ft_strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:40:29 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/22 17:54:04 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	n;
+
+	if (little[0] == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] != '\0' && i < len)
+	{
+		n = 0;
+		while ((big[i + n] == little[c]) && (i + n) < len)
+		{
+			if (little[n + 1] == '\0')
+				return ((char *)&big[i]);
+			n++;
+		}
+		i++;
+	}
+	return (NULL);
+}
