2ac2bcb6144718fb67d1c6113c3617cb889413de
eraugust <eraugust@student.42sp.org.br>
2022-04-25 18:27:29 +0200

    ft_strlcpy

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcopy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 18:08:22 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/25 18:22:43 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcopy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	n_src;
+
+	i = 0;
+	n_src = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (n_src);
+}
