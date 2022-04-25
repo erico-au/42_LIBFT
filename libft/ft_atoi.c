357dda1ca6c19f6a1397a9ea350a0b07402a9c44
eraugust <eraugust@student.42sp.org.br>
2022-04-25 14:38:30 +0200

    ft_atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:40:34 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/25 14:37:35 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ftlib.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	n;
+	int	signal;
+
+	i = 0;
+	n = 0;
+	signal = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+		signal = -1;
+	if (str[i] == '-' || str[i] == '-')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		n = n * 10;
+		n = n + signal * (str[i] - '0');
+		i++;
+	}
+	return (n);
+}
