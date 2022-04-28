4cd25991c50e0ccfe42cd65f860242261b0986c3
eraugust <eraugust@student.42sp.org.br>
2022-04-29 01:37:52 +0200

    ft_substr_1

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 20:31:58 by eraugust          #+#    #+#             */
-/*   Updated: 2022/04/29 00:08:52 by eraugust         ###   ########.fr       */
+/*   Updated: 2022/04/29 01:16:07 by eraugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	else if (start + len > ns)
 		len = ns - start;
 	str = (char *)ft_calloc((len + 1), sizeof(char));
-	if (target)
+	if (str)
 		ft_strlcpy(str, s + start, len + 1);
 	return (str);
 }
