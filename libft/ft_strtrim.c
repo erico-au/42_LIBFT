f88d185047eab1983216f0fe622e9623e77582ea
eraugust <eraugust@student.42sp.org.br>
2022-04-29 21:08:29 +0200

    ft_strtrim

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 01:02:25 by eraugust          #+#    #+#             */
+/*   Updated: 2022/04/29 21:06:36 by eraugust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	n;
+	char	*ret;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	n = ft_strlen(s1);
+	while (n && ft_strchr(set, s1[n]))
+		n--;
+	ret = ft_substr((char *)s1, 0, n + 1);
+	return (ret);
+}
