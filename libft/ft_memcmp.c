8981e7d00875834568a04be8aafb7239b7e56c7f
eraugust <eraugust@student.42sp.org.br>
2022-05-12 22:28:12 +0200

    ft_memcmp_1

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,21 +6,21 @@
 /*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:40:24 by eraugust          #+#    #+#             */
-/*   Updated: 2022/04/22 17:29:06 by eraugust         ###   ########.fr       */
+/*   Updated: 2022/05/02 17:38:24 by eraugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *str1, const void *str2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if ((unsigned char *)str1[i] != (unsigned char *)str2[i])
-			return (((unsigned char *)str1[i]) - ((unsigned char *)str2[i]));
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return ((((unsigned char *)s1)[i]) - (((unsigned char *)s2)[i]));
 		else
 			i++;
 	}
