7604c8d8f13e743880897778a328767990cd77d7
eraugust <eraugust@student.42sp.org.br>
2022-05-12 22:23:11 +0200

    ft_atoi_1

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,11 @@
 /*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:40:34 by eraugust          #+#    #+#             */
-/*   Updated: 2022/04/25 14:37:35 by eraugust         ###   ########.fr       */
+/*   Updated: 2022/05/02 19:23:01 by eraugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ftlib.h"
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] == '-')
 		signal = -1;
-	if (str[i] == '-' || str[i] == '-')
+	if (str[i] == '+' || str[i] == '-' )
 		i++;
 	while (ft_isdigit(str[i]))
 	{
